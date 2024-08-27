8773088fec3910f9699a92d95320429a0238d36e
nadeemahbaan <equbalnadim@gmail.com>
2024-08-27 18:46:21 +0530

    journey started

---
+++ basic/src/App.js
@@ -0,0 +1,12 @@
+import Try1 from "./try1";
+
+function App() {
+  return (
+    <>
+      <Try1 />
+      <h1>isko bolte hai fragment kyuki jsx ek hi element pass karna allow krta hai to hum fragment pass krke uske andar multiple fragment pass krte hai</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ basic/src/try1.js
@@ -0,0 +1,7 @@
+function Try1(){
+    return(
+        <h3>try to karuga ki kuch or render hota hai ya nahi</h3>
+    )
+}
+
+export default Try1
